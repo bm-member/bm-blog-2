65a885a1344127112359d9f98ed0656e1bf9ae6a
Thet Minn Htun <thetminnhtun92@gmail.com>
2019-11-10 11:32:35 +0630

    api crud

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,76 @@
 <?php
 
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Post;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('post', function() {
+    $posts = Post::orderBy('id', 'desc')->paginate(6);
+    return response()->json(compact('posts'));
+});
+
+Route::get('post/{id}', function($id) {
+
+    $post = Post::find($id);
+
+    if($post) {
+        return response()->json([
+           'post' => $post
+        ]);
+    }
+
+    return response()->json([
+        'msg' => 'A post does not exists.'
+    ], 404);
+});
+
+Route::post('post', function(Request $req) {
+    $post = new Post();
+    $post->title = $req->title;
+    $post->content = $req->content;
+    $post->user_id = 1;
+    $post->save();
+
+    return response()->json([
+        'msg' => 'A post was created successfully.'
+    ]);
+});
+
+Route::put('post/{id}', function(Request $req, $id) {
+
+
+    $post = Post::find($id);
+
+    if($post) {
+        $post->update($req->all());
+        $post->save();
+
+        return response()->json([
+            'msg' => 'A post was updated successfully.'
+        ]);
+    } 
+
+    return response()->json([
+        'msg' => 'A post does not exists.'
+    ], 404);
+    
+});
+
+Route::delete('post/{id}', function($id) {
+    $post = Post::find($id);
+
+    if($post) {
+        $post->delete();
+        return response()->json([
+            'msg' => 'A post was deleted successfully.'
+        ]);
+    }
+
+    return response()->json([
+        'msg' => 'A post does not exists.'
+    ], 404);
 });
